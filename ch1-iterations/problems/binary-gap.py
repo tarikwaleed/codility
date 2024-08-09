fa6feafa6f94b444db25f40105aa7d298f6e898a
tarikwaleed <tarikwaleed.tech@gmail.com>
2024-08-09 08:24:15 +0300

    :heavy_check_mark: BinaryGap

--- ch1-iterations/problems/binary-gap.py
+++ ch1-iterations/problems/binary-gap.py
@@ -1,6 +1,13 @@
-def DecimalToBinary(num):
-    return DecimalToBinary(num // 2) + str(num % 2) if num > 1 else str(num % 2)
-
-# # Example usage:
-binary_representation = DecimalToBinary(2)
-print(binary_representation)  # Output: 1010
+def solution(n):
+    ones_indeces=[]
+    binary=bin(n)[2:]
+    for index,digit in enumerate(binary):
+        if digit=='1':
+            ones_indeces.append(index)
+    max_diff=0
+    for i in range(len(ones_indeces)-1):
+        diff=ones_indeces[i+1]-ones_indeces[i]
+        max_diff=max(max_diff,diff)
+    return max_diff-1 if max_diff>0 else  0
+a=solution(1041)
+print(a)
